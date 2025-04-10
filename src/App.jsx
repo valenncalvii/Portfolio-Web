3cff045218c480be29e957c9de442770230c0341
ValennCalvii <calvicoronel@gmail.com>
2025-04-10 20:08:56 -0300

    agregado foto personal y cambios de posicion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 function App() {
+  const [hoverIndex, setHoverIndex] = useState(null);
+
   return (
     <main className="flex flex-col min-h-screen bg-primary">
       <header className="flex justify-evenly p-4 w-full">
@@ -50,7 +53,7 @@ function App() {
       </header>
       <section className="flex flex-row justify-center items-center h-screen">
         <div className="w-100 h-100">
-          <img src="/franco-colapinto.jpg" alt="Foto de perfil" />
+          <img src="/FotoPersonal2.jpeg" alt="Foto de perfil" />
         </div>
         <div className="w-100 h-100 flex flex-col ml-5">
           <section>
@@ -72,7 +75,7 @@ function App() {
           <div className="flex justify-end">
             <div className="w-[30%] flex items-end justify-end border text-font-special"></div>
           </div>
-          <section className="w-full h-full flex justify-evenly items-center mt-4">
+          <section className="w-full h-full flex justify-start items-center mt-4">
             <button className="p-3 rounded-md cursor-pointer font-font-family text-font-primary bg-font-special hover:bg-font-special/75 transition-colors duration-300">
               Descargar cv
             </button>
@@ -119,19 +122,25 @@ function App() {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-4xl w-full">
           {[
             {
+              name: "Simon Says",
               src: "/simomsays.jpg",
               alt: "Simon Says",
               link: "https://simon-says-sigma.vercel.app/",
+              desc: "Un juego de memoria donde debes repetir la secuencia de colores. Este proyecto fue un trabajo de mi facultad, fue realizado con React.",
             },
             {
+              name: "Zapas Heaven",
               src: "/zapas.jpg",
               alt: "Zapas Heaven",
               link: "https://zapas-heaven.netlify.app/",
+              desc: "Una tienda de zapatillas hecha con html, css y javascript. Este proyeto",
             },
             {
+              name: "Pricing Page",
               src: "/pricing.jpg",
               alt: "Pricing Page",
               link: "https://chipper-sprinkles-daa850.netlify.app/",
+              desc: "Una pagina de precios hecha con html, css y javascript. Este proyecto fue un reto de frontendmentor.io",
             },
           ].map((project, index) => (
             <a
@@ -140,6 +149,8 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
               className="block group rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition duration-300"
+              onMouseOver={() => setHoverIndex(index)}
+              onMouseLeave={() => setHoverIndex(null)}
             >
               <div className="relative w-full aspect-square">
                 <img
@@ -147,6 +158,16 @@ function App() {
                   alt={project.alt}
                   className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
                 />
+                {hoverIndex === index && (
+                  <div className="absolute inset-0 flex items-center justify-center hover:bg-black opacity-85 transition-colors duration-300 ease-in-out">
+                    <div className="flex flex-col items-center justify-center text-center p-4 text-font-primary font-font-family">
+                      <h1 className="text-2xl font-extrabold">
+                        {project.name}
+                      </h1>
+                      <p>{project.desc}</p>
+                    </div>
+                  </div>
+                )}
               </div>
             </a>
           ))}
@@ -158,12 +179,30 @@ function App() {
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-4xl w-full">
           {[
-            { src: "/html.png", alt: "HTML" },
-            { src: "/css.png", alt: "CSS" },
-            { src: "/js.png", alt: "JavaScript" },
-            { src: "/react.png", alt: "React" },
-            { src: "/tailwind.png", alt: "Tailwind CSS" },
-            { src: "/git.png", alt: "Git" },
+            {
+              src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+              alt: "HTML",
+            },
+            {
+              src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg",
+              alt: "CSS",
+            },
+            {
+              src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+              alt: "JavaScript",
+            },
+            {
+              src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+              alt: "React",
+            },
+            {
+              src: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg ",
+              alt: "Tailwind CSS",
+            },
+            {
+              src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+              alt: "Git",
+            },
           ].map((skill, index) => (
             <div
               key={index}
@@ -177,10 +216,70 @@ function App() {
           ))}
         </div>
       </section>
+      <section className="flex flex-col items-center justify-center px-4 py-12">
+        <h1 className="font-font-family text-4xl font-extrabold text-font-primary mb-8">
+          Un poco mas sobre mi
+        </h1>
+        <article className="max-w-2xl w-full text-center">
+          <div className="w-[30%] m-auto border mb-0 text-font-special"></div>
+          <p className="font-font-family font-light text-xl my-4 text-font-primary">
+            Soy un apasionado por la tecnología y el desarrollo web. Me encanta
+            aprender cosas nuevas y enfrentarme a nuevos desafíos. Siempre estoy
+            buscando mejorar mis habilidades y aprender nuevas tecnologías.
+          </p>
+          <div className="w-[30%] m-auto border mb-0 text-font-special"></div>
+        </article>
+      </section>
+      <section className="flex flex-col items-center justify-center px-4 py-12">
+        <article className="max-w-2xl w-full text-center">
+          <h1 className="font-font-family text-4xl font-extrabold text-font-primary mb-8">
+            Trabajemos juntos!!
+          </h1>
+          <p className="font-font-family font-light text-xl my-4 text-font-primary">
+            Si quieres saber más sobre mí, no dudes en contactarme. Estoy
+            disponible para trabajar en proyectos interesantes y desafiantes.
+          </p>
+          <div className="flex flex-row justify-center items-center mt-4">
+            <a href="#" className="p-3 text-font-primary cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="w-12 h-12 rounded-full p-2 text-font-primary stroke-1 hover:text-font-special transition-colors duration-300"
+                fill="currentColor"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+                />
+              </svg>
+            </a>
+            <a className="p-3 text-font-primary cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="w-12 h-12 p-2 text-font-primary stroke-1 hover:text-font-special transition-colors duration-300"
+                fill="currentColor"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+                />
+              </svg>
+            </a>
+          </div>
+        </article>
+      </section>
       <footer className="flex justify-center items-center p-4 text-font-primary">
-        <p className="font-font-family text-lg">
-          © 2023 Valentin Calvi Coronel. Todos los derechos reservados.
-        </p>
+        <h2 className="font-font-family text-lg">
+          Developed by
+          <strong className="text-font-special"> valenncalvii</strong>
+        </h2>
       </footer>
     </main>
   );
